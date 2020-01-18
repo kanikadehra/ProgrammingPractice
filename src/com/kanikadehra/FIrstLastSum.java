615e28416be1bdf5c71fbedafd2a3c425dbcbe25
kanika <dehrakanika12@gmail.com>
2020-01-18 20:34:31 +0000

    created first last sum

---
+++ src/com/kanikadehra/FIrstLastSum.java
@@ -0,0 +1,26 @@
+package com.kanikadehra;
+
+public class FIrstLastSum {
+
+    public static int sumFirstAndLastDigit(int number){
+
+        int firstDigit = 0;
+        int lastDigit = 0;
+
+        if(number<0){
+            return -1;
+        }
+
+        lastDigit = number % 10;
+
+        while (number!=0){
+            firstDigit = number% 10;
+            number = number/10;
+        }
+
+        return (firstDigit+lastDigit);
+
+
+    }
+
+}
--- src/com/kanikadehra/Main.java
+++ src/com/kanikadehra/Main.java
@@ -3,6 +3,8 @@ package com.kanikadehra;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(PalindromeNumber.isPalindrome(1331));
+        System.out.println(FIrstLastSum.sumFirstAndLastDigit(1234));
+
+
     }
 }
