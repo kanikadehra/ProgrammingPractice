8ecc30bd4c0b732d0e02d66830e1d158668a61d7
kanika <dehrakanika12@gmail.com>
2020-01-19 14:59:14 +0000

    EvenDigitSum class added

---
+++ src/com/kanikadehra/EvenDigitSum.java
@@ -0,0 +1,29 @@
+package com.kanikadehra;
+
+public class EvenDigitSum {
+
+    public static int getEvenDigitSum(int number) {
+
+        int sum = 0;
+        int digit = 0;
+
+        if (number< 0){
+
+            return -1;
+        }
+
+        while (number >0){
+
+            digit = number % 10;
+
+            if(digit %2==0){
+               sum = sum+digit;
+           }
+
+           number = number/10;
+        }
+
+        return sum;
+    }
+
+}
--- src/com/kanikadehra/Main.java
+++ src/com/kanikadehra/Main.java
@@ -3,7 +3,7 @@ package com.kanikadehra;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(FIrstLastSum.sumFirstAndLastDigit(1234));
+        System.out.println(EvenDigitSum.getEvenDigitSum(-342));
 
 
     }
